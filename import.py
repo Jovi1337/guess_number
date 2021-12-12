4e3f8c68108b47966ec8cdedda5fb5ff3ff64ce2
Jovi <shock10213@gmail.com>
2021-12-12 22:31:37 +0800

    new_version_with_count_function

--- import.py
+++ import.py
@@ -5,14 +5,18 @@ import random
 r = random.randint(1, 100)
 # randint = random integer
 
+count = 0 	# 猜了幾次
+
 while True:
+	count = count + 1 	# count += 1 也是一樣意思
 	ans = input('請猜一個數字: ')		# input會把所有東西存成字串
 	ans = int(ans)					# 所以要 int
 	if ans == r:
 		print('恭喜答對!!!   ')
+		print('這是你猜的第', count, '次')
 		break
 	elif ans > r:
 		print('你猜得比答案大')
 	elif ans < r:
 		print('你猜得比答案小')
-		
+	print('這是你猜的第', count, '次')
